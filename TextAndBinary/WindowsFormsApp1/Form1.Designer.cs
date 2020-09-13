21c1860000646c4a15f53c8f9e3d4783709f596d
Karsten Jeppesen <karsten@jeppesens.com>
2020-09-13 10:04:10 +0200

    Possible solution to binary to text and back exercise

---
+++ TextAndBinary/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbText = new System.Windows.Forms.TextBox();
+            this.tbBinary = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbText
+            // 
+            this.tbText.Location = new System.Drawing.Point(19, 89);
+            this.tbText.Name = "tbText";
+            this.tbText.Size = new System.Drawing.Size(364, 22);
+            this.tbText.TabIndex = 0;
+            this.tbText.TextChanged += new System.EventHandler(this.tbText_TextChanged);
+            // 
+            // tbBinary
+            // 
+            this.tbBinary.Location = new System.Drawing.Point(19, 159);
+            this.tbBinary.Name = "tbBinary";
+            this.tbBinary.Size = new System.Drawing.Size(364, 22);
+            this.tbBinary.TabIndex = 1;
+            this.tbBinary.TextChanged += new System.EventHandler(this.tbBinary_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Binary";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbBinary);
+            this.Controls.Add(this.tbText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbText;
+        private System.Windows.Forms.TextBox tbBinary;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TextAndBinary/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,50 @@
+﻿//
+// Microsoft documentation to the rescue
+// https://docs.microsoft.com/en-us/dotnet/api/system?view=netframework-4.8
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tbText_TextChanged(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in tbText.Text.ToCharArray())
+            {
+                sb.Append(Convert.ToString(c, 2).PadLeft(8, '0'));
+            }
+            tbBinary.Text = sb.ToString();
+            tbBinary.Update();
+        }
+
+        private void tbBinary_TextChanged(object sender, EventArgs e)
+        {
+            int length8 = (tbBinary.Text.Length / 8) * 8;
+            if (length8 > 0)
+            {
+                List<byte> byteList = new List<Byte>();
+                for (int i = 0; i < length8; i += 8)
+                {
+                    byteList.Add(Convert.ToByte(tbBinary.Text.Substring(i, 8), 2));
+                }
+                tbText.Text = Encoding.ASCII.GetString(byteList.ToArray());
+                tbText.Update();
+            }
+        }
+    }
+}
