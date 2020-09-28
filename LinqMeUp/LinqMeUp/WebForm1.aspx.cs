48170be25e7ee4f677b687f4e65f4f58a66513b1
Karsten Jeppesen <karsten@jeppesens.com>
2020-09-28 12:17:26 +0200

    LINQ demo

---
+++ LinqMeUp/LinqMeUp/WebForm1.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LinqMeUp
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ElephantDataContext dbContext = new ElephantDataContext();
+            GridView1.DataSource = from c in dbContext.theStats where c.countrycode.Contains("JP") select c;
+            GridView1.DataBind();
+        }
+    }
+}
