9875b1478fd70b4e79fef8f7425319f398edfeae
Karsten Jeppesen <karsten@jeppesens.com>
2020-10-31 13:58:50 +0100

    SingleSignOnWIthAzureAD, SSO demo
    Shows how to use ADDS and SSO

---
+++ SingleSignOnWIthAzureAD/SingleSignOnWIthAzureAD/App_Start/Startup.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Owin; /* Added*/
+using Microsoft.Owin.Security; /* Added*/
+using Microsoft.Owin.Security.Cookies; /* Added*/
+using Microsoft.Owin.Security.OpenIdConnect; /* Added*/
+using Owin; /* Added*/
+using System;
+using System.Collections.Generic;
+using System.Configuration; /* Added*/
+using System.Globalization; /* Added*/
+using System.Linq;
+using System.Threading.Tasks; /* Added*/
+using System.Web;
+
+[assembly: OwinStartup(typeof(SingleSignOnWIthAzureAD.App_Start.Startup))]
+namespace SingleSignOnWIthAzureAD.App_Start
+{
+    public class Startup
+    {
+        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private static string aadInstance = ConfigurationManager.AppSettings["ida:AAdInstance"];
+        private static string tenant = ConfigurationManager.AppSettings["ida:Tenant"];
+        private static string postLogoutRedirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"];
+
+        // Get globalized string for the authority
+        // Will result in values from the Web.config file: https://login.microsoftonline.com/indsætDitDomainHer.onmicrosoft.com
+        string authority = String.Format(CultureInfo.InvariantCulture, aadInstance, tenant);
+
+        public void Configuration(IAppBuilder app)
+        {
+            DoAuthentication(app);
+        }
+
+        public void DoAuthentication(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    ClientId = clientId,
+                    Authority = authority,
+                    PostLogoutRedirectUri = postLogoutRedirectUri,
+                    Notifications = new OpenIdConnectAuthenticationNotifications
+                    {
+                        AuthenticationFailed = context =>
+                        {
+                            context.HandleResponse();
+                            context.Response.Redirect("/Error/message=" + context.Exception.Message);
+                            return Task.FromResult(0);
+                        }
+                    }
+                }
+                );
+        }
+    }
+}
---
+++ SingleSignOnWIthAzureAD/SingleSignOnWIthAzureAD/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Owin.Security; /* Added */
+using Microsoft.Owin.Security.Cookies; /* Added */
+using Microsoft.Owin.Security.OpenIdConnect; /* Added */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SingleSignOnWIthAzureAD.Controllers
+{
+    public class AccountController : Controller
+    {
+        public void SignIn()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                HttpContext.GetOwinContext().Authentication.Challenge(
+                    new AuthenticationProperties { RedirectUri = "/" },
+                        OpenIdConnectAuthenticationDefaults.AuthenticationType
+                    );
+            }
+        }
+
+        public void SignOut()
+        {
+            HttpContext.GetOwinContext().Authentication.SignOut(
+                OpenIdConnectAuthenticationDefaults.AuthenticationType,
+                CookieAuthenticationDefaults.AuthenticationType
+                );
+        }
+    }
+}
