4c46e49c24567dd25f7974557fcbff9ed726c984
Karsten Jeppesen <karsten@jeppesens.com>
2020-11-14 11:46:44 +0100

    Individual Account Example
    Demonstrates the built-in authentication. Video included in directory

---
+++ IndividualAccount/IndividualAccount/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace IndividualAccount.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ IndividualAccount/IndividualAccount/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace IndividualAccount.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
